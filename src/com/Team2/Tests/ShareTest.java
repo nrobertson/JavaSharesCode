b983f56ba97be58fa67dc569d126c83e42808cb2
nrobertson <n.robertson@dundee.ac.uk>
2012-11-08 13:47:23 +0000

    added in ShareTest class

---
+++ src/com/Team2/Tests/ShareTest.java
@@ -0,0 +1,14 @@
+package com.Team2.Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ShareTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
