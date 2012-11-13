62b988d2d8254044c99fc4bf7aab99e00ad25965
Neil Fordyce <nfordyce@dundee.ac.uk>
2012-11-13 13:29:00 +0000

    Tests to make sure exceptions are caught properly in Porfolio

--- src/com/Team2/Tests/ShareTest.java
+++ src/com/Team2/Tests/ShareTest.java
@@ -38,10 +38,6 @@ public class ShareTest {
 	public void testPositiveGetWeekPercentChange() {
 		populateApiData(YahooFinanceAPI.StockSymbol.BP, "428", MONDAY, "400");
 		
-		testFunction(7);
-	}
-
-	private void testFunction(int expectedValue) {
 		int rounded1=0;
 		try {
 			rounded1 = Math.round(share.getWeekPercentChange());
@@ -50,7 +46,7 @@ public class ShareTest {
 			e.printStackTrace();
 		}
 		
-		assertEquals(rounded1, expectedValue);
+		assertEquals(rounded1, 7);
 	}
 	
 	@Test
