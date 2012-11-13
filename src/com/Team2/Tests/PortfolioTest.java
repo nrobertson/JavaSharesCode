0b24dbd3220ac4a9cd05514b947c73aaa119f875
Neil Fordyce <nfordyce@dundee.ac.uk>
2012-11-13 13:26:39 +0000

    Tests to check exceptions are being caught in Portfolio

--- src/com/Team2/Tests/PortfolioTest.java
+++ src/com/Team2/Tests/PortfolioTest.java
@@ -10,7 +10,7 @@ import com.example.shares.YahooFinanceAPI;
 
 public class PortfolioTest {
 	
-	static final String MONDAY = "2012-11-05"; 
+	static final String MONDAY = "2012-11-12"; 
 
 	@Before
 	public void setUp() throws Exception {
@@ -23,17 +23,32 @@ public class PortfolioTest {
 	public void testGetPercentageChange() {
 		ShareTest.populateApiData(YahooFinanceAPI.StockSymbol.BP, "400", MONDAY, "400");
 		ShareTest.populateApiData(YahooFinanceAPI.StockSymbol.EXPN, "400", MONDAY, "400");
-		ShareTest.populateApiData(YahooFinanceAPI.StockSymbol.HSBA, "400", MONDAY, "400");
+		ShareTest.populateApiData(YahooFinanceAPI.StockSymbol.HSBA, "440", MONDAY, "400");
 		ShareTest.populateApiData(YahooFinanceAPI.StockSymbol.SN, "400", MONDAY, "400");
-		ShareTest.populateApiData(YahooFinanceAPI.StockSymbol.MKS, "400", MONDAY, "400");
+		ShareTest.populateApiData(YahooFinanceAPI.StockSymbol.MKS, "400", MONDAY, "800");
 		
 		String[] change = Portfolio.getPercentageChange();
 		
 		assertEquals(change[0], "BP: 0%");
 		assertEquals(change[1], "Experian: 0%");
-		assertEquals(change[2], "HSBC: 0%");
-		assertEquals(change[3], "M&S: 0%");
+		assertEquals(change[2], "HSBC: 10%");
+		assertEquals(change[3], "M&S: -50%");
 		assertEquals(change[4], "S&N: 0%");
 	}
+	
+	@Test
+	public void testGetInvalidPercentageChange() {
+		ShareTest.populateApiData(YahooFinanceAPI.StockSymbol.BP, "400", MONDAY, "0");
+		ShareTest.populateApiData(YahooFinanceAPI.StockSymbol.EXPN, "0", MONDAY, "400");
+		ShareTest.populateApiData(YahooFinanceAPI.StockSymbol.HSBA, "-1", MONDAY, "400");
+		
+		String[] change = Portfolio.getPercentageChange();
+		
+		assertEquals(change[0], "BP: N/A for week");
+		assertEquals(change[1], "Experian: unavailable");
+		assertEquals(change[2], "HSBC: unavailable");
+	}
+	
+	
 
 }
